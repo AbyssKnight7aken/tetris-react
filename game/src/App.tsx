1f33420bb92f710b1791204f349f95eaa7844508
AbyssKnight7aken <7akenbg@gmail.com>
2023-11-21 18:40:01 +0200

    setup react-router-dom

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -1,10 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Game from './components/Game/Game';
 
 function App() {
   return (
     <div className="App">
-      <Game rows={20} columns={10} />
+      <Routes>
+        <Route path='/' element={<Game rows={20} columns={10} />}/>
+      </Routes>
     </div>
   );
 }
