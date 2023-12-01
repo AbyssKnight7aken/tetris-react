bf44b812b1d4cf0dd9820fe9950e16df7fd4e9dc
AbyssKnight7aken <7akenbg@gmail.com>
2023-12-01 21:47:34 +0200

    create home component

---
+++ game/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Game from '../Game/Game';
+
+
+const Home = () => {
+    return (
+        <>
+            <Game rows={20} columns={10} />
+            <p>Home</p>
+        </>
+    );
+}
+
+export default Home;
