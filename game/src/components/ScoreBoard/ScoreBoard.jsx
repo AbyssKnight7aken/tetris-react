69f86698a230e7b92bbda2b91b9d2a1a1c76838a
AbyssKnight7aken <7akenbg@gmail.com>
2023-12-04 23:32:27 +0200

    add ScoreBoard component

--- game/src/components/ScoreBoard/ScoreBoard.jsx
+++ game/src/components/ScoreBoard/ScoreBoard.jsx
@@ -1,6 +1,20 @@
+import Card from '../common/Card/Card';
+import { useGameContext } from '../../contexts/gameContext'
+
+import './ScoreBoard.css';
+
 const ScoreBoard = () => {
+    const { scores } = useGameContext();
+    //const scores = [];
     return (
-        <p>ScoreBoard</p>
+        <section className="scores">
+            <h1>Scores</h1>
+            {
+                scores.length === 0
+                    ? <h3>No scores yet</h3>
+                    : scores.map(x => <Card key={x._id} score={x} />)
+            }
+        </section>
     );
 }
 
